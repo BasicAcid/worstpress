748870e02fb10ac996a76a670643b9e687890753
BasicAcid <david.tabarie@gmail.com>
2024-01-23 19:22:23 +0100

    Checkpoint.

--- worstpress.c
+++ worstpress.c
@@ -25,12 +25,36 @@ struct FreqNode
     struct FreqNode *right;
 };
 
+struct IntArray
+count_freq(const char *input)
+{
+    struct IntArray freq_arr = create_dynamic_array(100);
+
+    while(*input != '\0')
+    {
+        freq_arr.data[ (unsigned char)*input ]++;
+        input++;
+    }
+    return freq_arr;
+}
+
+void
+print_dynamic_array(struct IntArray arr)
+{
+    for(size_t i = 0; i < arr.size; i++)
+        printf("%f\n", arr.data[i]);
+}
 
 int
 main()
 {
+    //const char *input = "This is some kind of input with some redudancies I guess you know man, like, seee.";
 
+    const char *input = "hello";
 
+    struct IntArray better_freqs = count_freq(input);
+
+    print_dynamic_array(better_freqs);
 
     return 0;
 }
